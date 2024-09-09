7c2876f0c0da8ce8fd5866e4d2a60d6a6cd27797
Aryan <aryanpandita003@gmail.com>
2024-09-09 11:48:53 +0530

    newone

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+//new
+
 import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
+import { WagmiConfig, createConfig} from 'wagmi';
+import { mainnet, polygon, optimism} from 'wagmi/chains'
+import { arbitrum } from 'viem/chains'
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { createWeb3Modal, defaultWagmiConfig} from '@web3modal/wagmi/react';
+import { Web3Button, Web3Modal } from '@web3modal/react';
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+
+
+
+// 1. Get projectId
+const projectId = '023a4616f3bb8987407f5c86490dc9f9';
+if(!projectId){
+  throw new Error("pls provide project id");
+}
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  
+};
+
+const chains = [mainnet, polygon, optimism, arbitrum];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains ,  themeVariables: {
+  '--w3m-color-mix': '#00308F',
+  '--w3m-color-mix-strength': 40
+}});
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w3m-button'>
+  <WagmiConfig config={wagmiConfig} >
+      <w3m-button />
+
+    </WagmiConfig>
+
     </div>
+    
   );
 }
 
 export default App;
+
+
+
+
+
+
